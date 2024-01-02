cee7816913f4f3a98f801f1c6425922be5fb6ce2
rea2001 <rea2001>
2024-01-02 18:43:31 -0500

    Clases

--- src/app/Modelos/Entidades.model.ts
+++ src/app/Modelos/Entidades.model.ts
@@ -2,13 +2,14 @@ import { ControlContainer } from "@angular/forms";
 
 export class Vivienda {
     constructor(id_Vivienda?: number, NombreVivienda?: string, Precio?: number, TipoPropiedad?: string, Latitud?: number,
-        Longitud?: number, Estado?: string, IdCaracteristica?: number, IdServicio?: number, IdCondicion?: number, IdUbicacion?: number, IdUsuario?: number) {
+        Longitud?: number, Descripcion?: string, Estado?: string, IdCaracteristica?: number, IdServicio?: number, IdCondicion?: number, IdUbicacion?: number, IdUsuario?: number) {
         this.Id_Viv = id_Vivienda;
         this.Nombre = NombreVivienda;
         this.Precio = Precio;
         this.Tip_Pro = TipoPropiedad;
         this.Latitud = Latitud;
         this.Longitud = Longitud;
+        this.Descripcion = Descripcion;
         this.Estado = Estado;
         this.Id_Car_Per = IdCaracteristica;
         this.Id_Ser_Per = IdServicio;
@@ -22,6 +23,7 @@ export class Vivienda {
     Tip_Pro?: string = "";
     Latitud?: number = 0;
     Longitud?: number = 0;
+    Descripcion?: string  = "";
     Estado?: string = "Disponible"
     Id_Car_Per?: number = 0;
     Id_Ser_Per?: number = 0;
@@ -30,13 +32,103 @@ export class Vivienda {
     Id_Usu_Per?: number = 0;
 }
 
-export class caracteristicas{
+export class Caracteristicas{
     constructor(Id_Car?: number, Num_Ban?: number, Num_Hab?:number, Num_Pis?:number,Metraje?:number, Cocina?: boolean, Sala?:boolean,Comedor?: boolean,
         Lavanderia?: boolean, Ascensor?: boolean,Amoblado?: boolean,Jardin?: boolean,Terraza?: boolean){
             this.Id_Car = Id_Car;
+            this.Num_Ban = Num_Ban;
+            this.Num_Hab = Num_Hab;
+            this.Num_Pis = Num_Pis;
+            this.Metraje = Metraje;
+            this.Cocina = Cocina;
+            this.Sala = Sala;
+            this.Comedor = Comedor;
+            this.Lavanderia = Lavanderia;
+            this.Ascensor = Ascensor;
+            this.Amoblado = Amoblado;
+            this.Jardin = Jardin;
+            this.Terraza = Terraza;
     }
 
     Id_Car?: number = 0;
+    Num_Ban?: number = 0;
+    Num_Hab?: number = 0;
+    Num_Pis?: number = 0;
+    Metraje?: number= 0;
+    Cocina?: boolean = false;
+    Sala?: boolean = false;
+    Comedor?: boolean = false;
+    Lavanderia?: boolean = false;
+    Ascensor?: boolean = false;
+    Amoblado?: boolean = false;
+    Jardin?: boolean = false;
+    Terraza?: boolean = false;
+
+}
+
+export class Servicios{
+    constructor(Id_Ser?:number, Luz?: boolean, Agua?: boolean, Internet?: boolean,Garaje?: boolean,Telefono?: boolean,
+        Calefon?: boolean,Duc_Elec?: boolean){
+            this.Id_Ser = Id_Ser;
+            this.Agua = Agua;
+            this.Luz = Luz;
+            this.Internet = Internet;
+            this.Garaje = Garaje;
+            this.Telefono = Telefono;
+            this.Calefon = Calefon;
+            this.Duc_Elec = Duc_Elec;
+        }
+
+    Id_Ser?: number = 0;
+    Luz?: boolean = false;
+    Agua?: boolean = false;
+    Garaje?: boolean = false;
+    Internet?: boolean = false;
+    Telefono?: boolean = false;
+    Calefon?: boolean = false;
+    Duc_Elec?: boolean = false;
+}
+
+export class Condiciones{
+    constructor(Id_Con?: number, Num_Pers?: number, Mascotas?: boolean, Fiestas?: boolean, Garantia?:number ){
+        this.Id_Con = Id_Con;
+        this.Num_Pers = Num_Pers;
+        this.Garantia = Garantia;
+        this.Mascotas = Mascotas;
+        this.Fiestas = Fiestas;
+    }
+
+    Id_Con?: number = 0;
+    Garantia?: number = 0;
+    Num_Pers?: number = 0;
+    Mascotas?: boolean = false;
+    Fiestas?: boolean = false;
+}
+
+export class ubicacion{
+    constructor(Id_Ubi?: number, Provincia?: string, Canton?: string, Parroquia?: string){
+        this.Id_Ubi = Id_Ubi;
+        this.Provincia = Provincia;
+        this.Canton = Canton;
+        this.Parroquia = Parroquia;
+    }
+    Id_Ubi?: number = 0;
+    Provincia?: string  = "";
+    Canton?: string  = "";
+    Parroquia?: string  = "";
+}
+
+export class Fotos{
+    constructor(Id_Fot?:number, Id_Viv_Per?: number, Descripcion?: string, Foto_Com?: string){
+        this.Id_Fot = Id_Fot;
+        this.Id_Viv_Per = Id_Viv_Per;
+        this.Descripcion = Descripcion;
+        this.Foto_Com = Foto_Com;
+    }
+    Id_Fot?: number = 0;
+    Id_Viv_Per?: number = 0;
+    Descripcion?: string  = "";
+    Foto_Com?: string  = "";
 }
 
 //-------------------------------------------------------------------------------------
